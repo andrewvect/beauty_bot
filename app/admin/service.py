e04d30295bbf3a736a27075a36c1df66f8c4ecbc
andrewvect <andrewvect@gmail.com>
2024-01-29 16:36:05 +0100

    code refactoring

--- app/admin/service.py
+++ app/admin/service.py
@@ -1,6 +1,4 @@
-import traceback
-
-from telebot import types
+from beauty_bot.app.admin.tools import send_menu_with_photo, edit_message
 from beauty_bot.extantions import bot
 from beauty_bot.app.keyboards import menu_with_questionaries, admin_keyboard2
 
@@ -13,49 +11,16 @@ def create_message_for_questionary(menu):
     return message
 
 
-def send_menu_with_photo(call, type, menu):
-    with open(f"beauty_bot/app/photos/{menu.master_data['url_to_photo']}.jpg", 'rb') as photo:
-        bot.send_photo(call.message.chat.id,
-                       caption=create_message_for_questionary(menu),
-                       photo=photo,
-                       reply_markup=menu_with_questionaries(menu, type))
-
-
-def edit_menu_with_photo(call, type, menu):
-    with open(f"beauty_bot/app/photos/{menu.master_data['url_to_photo']}.jpg", 'rb') as photo:
-        bot.edit_message_media(media=types.InputMedia(type='photo', media=photo),
-                               chat_id=call.message.chat.id,
-                               message_id=call.message.id,
-                               reply_markup=menu_with_questionaries(menu, type))
-
-
-def edit_message_caption(call, menu):
-    bot.edit_message_caption(caption=create_message_for_questionary(menu), chat_id=call.message.chat.id,
-                             message_id=call.message.id,
-                             reply_markup=menu_with_questionaries(menu, type))
-
-
 def send_menu_with_questionaries(call, type, state, menu):
     try:
 
         menu.get_data_for_questionary()
 
         if state == 'new':
-            send_menu_with_photo(call, type, menu)
+            send_menu_with_photo(call, type, menu, menu_with_questionaries, create_message_for_questionary(menu))
 
         if state == 'old':
-            try:
-                edit_menu_with_photo(call, type, menu)
-
-            except Exception:
-                pass
-
-            try:
-
-                edit_message_caption(call, menu)
-
-            except Exception:
-                pass
+            edit_message(call, menu, menu_with_questionaries, create_message_for_questionary(menu), type)
 
     except Exception:
         bot.send_message(call.message.chat.id, "Не создано еще ни одной анкеты.")

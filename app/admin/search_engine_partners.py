5b18fbd4999616465f823be1c2265ffc83ed7a03
andrewvect <andrewvect@gmail.com>
2024-01-29 16:20:23 +0100

    code refactoring

--- app/admin/search_engine_partners.py
+++ app/admin/search_engine_partners.py
@@ -3,6 +3,7 @@ import traceback
 from sqlalchemy.orm import sessionmaker
 from telebot import types
 
+from beauty_bot.app.admin.tools import send_menu_with_photo, edit_message
 from beauty_bot.app.apps_tools.message_deleter import delete_previous_messages
 from beauty_bot.app.db_queries import set_master_active_profile
 from beauty_bot.extantions import bot
@@ -30,44 +31,21 @@ def profiles_partners_by_city(call):
         menu_with_partners.get_partners_ids_by_city_name()
         send_menu_with_partners_questionaries(call, 'new')
     except Exception:
-        print(traceback.print_exc())
+
         bot.send_message(call.message.chat.id, 'В выбранном городе еще нет анкет.')
         send_cites_to_find_partners_profiles(call)
 
 
 def send_menu_with_partners_questionaries(call, state):
     menu_with_partners.get_data_for_questionary()
-    data = menu_with_partners.master_data
 
     if state == 'new':
-        with open(f"beauty_bot/app/photos/{data['url_to_photo']}.jpg", 'rb') as photo:
-            bot.send_photo(chat_id=call.message.chat.id, photo=photo,
-                           reply_markup=menu_with_questionaries_search_by_city(data['is_active'],
-                                                                               menu_with_partners.count_numbers(),
-                                                                               'P',
-                                                                               data['id']),
-                           caption=menu_with_partners.get_description())
+        send_menu_with_photo(call, 'P', menu_with_partners, menu_with_questionaries_search_by_city,
+                             menu_with_partners.get_description())
 
     if state == 'old':
-        try:
-            with open(f"beauty_bot/app/photos/{data['url_to_photo']}.jpg", 'rb') as photo:
-                bot.edit_message_media(media=types.InputMedia(type='photo', media=photo),
-                                       chat_id=call.message.chat.id,
-                                       message_id=call.message.id,
-                                       reply_markup=menu_with_questionaries_search_by_city(data['is_active'],
-                                                                                           menu_with_partners.count_numbers(),
-                                                                                           'P',
-                                                                                           data['id']))
-
-                bot.edit_message_caption(caption=menu_with_partners.get_description(),
-                                         chat_id=call.message.chat.id,
-                                         message_id=call.message.id,
-                                         reply_markup=menu_with_questionaries_search_by_city(data['is_active'],
-                                                                                             menu_with_partners.count_numbers(),
-                                                                                             'P',
-                                                                                             data['id']))
-        except Exception:
-            print(traceback.print_exc())
+        edit_message(call, menu_with_partners, menu_with_questionaries_search_by_city,
+                     menu_with_partners.get_description(), 'P')
 
 
 def next_page_admin_partner_menu_by_city(call):

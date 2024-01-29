497f2c6a8b3ebd49bc8cc9906f98862249f73d35
andrewvect <andrewvect@gmail.com>
2024-01-29 14:30:03 +0100

    add funtion to save files

---
+++ app/apps_tools/file_saver.py
@@ -0,0 +1,18 @@
+from beauty_bot.app.services import generate_key
+
+
+def save_image_and_get_path(message, bot) -> str:
+    photo = message.photo[-1]
+    file_id = photo.file_id
+
+    file_info = bot.get_file(file_id)
+    file_path = file_info.file_path
+
+    downloaded_file = bot.download_file(file_path)
+
+    file_name = generate_key(7)
+
+    with open(f'beauty_bot/app/photos/{file_name}.jpg', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    return file_name + '.jpg'

82eee1ad5101444bd04000d5771c3f4567c3bf76
andrewvect <andrewvect@gmail.com>
2024-01-29 15:18:08 +0100

    refactoring add delete_messages decorator

--- app/admin/menu_with_masters.py
+++ app/admin/menu_with_masters.py
@@ -1,8 +1,7 @@
-
 from sqlalchemy.orm import sessionmaker
 
-
 from beauty_bot.app.admin.service import send_menu_with_questionaries
+from beauty_bot.app.apps_tools.message_deleter import delete_previous_messages
 from beauty_bot.app.db_queries import set_master_active_profile
 from beauty_bot.extantions import bot
 from beauty_bot.app.keyboards import admin_keyboard2
@@ -14,12 +13,13 @@ queries_to_db = QueriesToDb(Session)
 menu_with_masters = AdminMenuMasters(queries_to_db)
 
 
-def admin_logout(call):
-    bot.delete_message(call.message.chat.id, call.message.id)
+@delete_previous_messages
+def admin_logout(call) -> None:
+    pass
 
 
+@delete_previous_messages
 def return_to_admin_menu(call):
-    bot.delete_message(call.message.chat.id, call.message.id)
     bot.send_message(call.message.chat.id, "Меню админа", reply_markup=admin_keyboard2())
 
 
@@ -29,33 +29,24 @@ def send_menu_with_masters(call):
 
 
 def next_page_masters_admin_menu(call):
-
     menu_with_masters.up_page()
-    print(menu_with_masters.content_ids)
 
     send_menu_with_questionaries(call, call.data[len('ad_next_page_'):], 'old', menu_with_masters)
 
 
 def previous_page_masters_admin_menu(call):
-
     menu_with_masters.down_page()
 
     send_menu_with_questionaries(call, call.data[len('ad_previous_page_'):], 'old', menu_with_masters)
 
 
+@delete_previous_messages
 def back_to_admin_menu_from_questionaries(call):
-    try:
-        bot.delete_message(call.message.chat.id, call.message.id)
-        bot.delete_message(call.message.chat.id, call.message.id - 1)
-    except Exception:
-        pass
-
     bot.send_message(call.message.chat.id, "Меню админа:", reply_markup=admin_keyboard2())
 
 
+@delete_previous_messages
 def back_to_admin_menu_create_questionary(call):
-    bot.delete_message(call.message.chat.id, call.message.id)
-    bot.delete_message(call.message.chat.id, call.message.id - 1)
 
     bot.send_message(call.message.chat.id, "Меню админа:", reply_markup=admin_keyboard2())
 
@@ -65,4 +56,3 @@ def change_master_visibility(call):
     set_master_active_profile(master_id)
 
     send_menu_with_questionaries(call, 'masters', 'old', menu_with_masters)
-

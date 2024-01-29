d09c5579acbe581e385d07a2d5a4b712a30eda75
andrewvect <andrewvect@gmail.com>
2024-01-29 14:29:41 +0100

    add config class and logger

--- app/config.py
+++ app/config.py
@@ -1,3 +1,27 @@
-admin_key = 'one2six13'
-bot_key = '5829140371:AAE-VgcQJaUaaOeNiHgP_x2Hu2kblyh_ZQo'
-bot_name_tg = '@OneBeauty_bot'
+import logging
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+DOTENV = os.path.join(os.path.dirname(__file__), ".env")
+
+
+class AppConfig(BaseSettings):
+    admin_key: str
+    bot_key: str
+    bot_name_tg: str
+
+    model_config = SettingsConfigDict(env_file=DOTENV)
+
+
+CONFIG = AppConfig()
+
+# Logging.
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+handler = logger.handlers and logger.handlers[0] or logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(levelname)s [%(filename)s:%(lineno)s] %(message)s'))
+if not logger.handlers:
+    logger.addHandler(handler)
+

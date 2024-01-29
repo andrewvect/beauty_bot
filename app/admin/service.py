bb10929e8200da3b42c3acd9c05d8df391183e02
andrewvect <andrewvect@gmail.com>
2024-01-29 15:32:31 +0100

    refactoring function

--- app/admin/service.py
+++ app/admin/service.py
@@ -5,49 +5,58 @@ from beauty_bot.extantions import bot
 from beauty_bot.app.keyboards import menu_with_questionaries, admin_keyboard2
 
 
+def create_message_for_questionary(menu):
+    message = f"{menu.masted_data['description']} \n" \
+              f"Город: {menu.city_name.capitalize()} \n" \
+              f"Район города: {menu.city_name.capitalize()} \n"
+
+    return message
+
+
+def send_menu_with_photo(call, type, menu):
+    with open(f"beauty_bot/app/photos/{menu.master_data['url_to_photo']}.jpg", 'rb') as photo:
+        bot.send_photo(call.message.chat.id,
+                       caption=create_message_for_questionary(menu),
+                       photo=photo,
+                       reply_markup=menu_with_questionaries(menu, type))
+
+
+def edit_menu_with_photo(call, type, menu):
+    with open(f"beauty_bot/app/photos/{menu.master_data['url_to_photo']}.jpg", 'rb') as photo:
+        bot.edit_message_media(media=types.InputMedia(type='photo', media=photo),
+                               chat_id=call.message.chat.id,
+                               message_id=call.message.id,
+                               reply_markup=menu_with_questionaries(menu, type))
+
+
+def edit_message_caption(call, menu):
+    bot.edit_message_caption(caption=create_message_for_questionary(menu), chat_id=call.message.chat.id,
+                             message_id=call.message.id,
+                             reply_markup=menu_with_questionaries(menu, type))
+
+
 def send_menu_with_questionaries(call, type, state, menu):
     try:
 
         menu.get_data_for_questionary()
-        data = menu.master_data
-
-        message = f"{data['description']} \n" \
-                  f"Город: {menu.city_name.capitalize()} \n" \
-                  f"Район города: {menu.city_name.capitalize()} \n"
 
         if state == 'new':
-            with open(f"beauty_bot/app/photos/{data['url_to_photo']}.jpg", 'rb') as photo:
-                bot.send_photo(call.message.chat.id,
-                               caption=message,
-                               photo=photo,
-                               reply_markup=menu_with_questionaries(data['is_active'],
-                                                                    menu.count_numbers(),
-                                                                    type,
-                                                                    data['id']))
+            send_menu_with_photo(call, type, menu)
 
         if state == 'old':
             try:
-                with open(f"beauty_bot/app/photos/{data['url_to_photo']}.jpg", 'rb') as photo:
-
-                    bot.edit_message_media(media=types.InputMedia(type='photo', media=photo),
-                                           chat_id=call.message.chat.id,
-                                           message_id=call.message.id,
-                                           reply_markup=menu_with_questionaries(data['is_active'],
-                                                                                menu.count_numbers(), type,
-                                                                                data['id']))
+                edit_menu_with_photo(call, type, menu)
 
             except Exception:
-                print(traceback.print_exc())
+                pass
+
             try:
 
-                bot.edit_message_caption(caption=message, chat_id=call.message.chat.id, message_id=call.message.id,
-                                         reply_markup=menu_with_questionaries(data['is_active'],
-                                                                              menu.count_numbers(), type,
-                                                                              data['id']))
+                edit_message_caption(call, menu)
+
             except Exception:
-                print(traceback.print_exc())
+                pass
 
     except Exception:
-        print(traceback.print_exc())
         bot.send_message(call.message.chat.id, "Не создано еще ни одной анкеты.")
         bot.send_message(call.message.chat.id, "Меню админа", reply_markup=admin_keyboard2())

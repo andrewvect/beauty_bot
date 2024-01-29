4cf6bb550d1eba1be56c451edaea16feb539f9d6
andrewvect <andrewvect@gmail.com>
2024-01-29 15:18:00 +0100

    refactoring

--- app/keyboards.py
+++ app/keyboards.py
@@ -168,15 +168,18 @@ def keyboard_to_srart_mailing():
     return inline_full
 
 
-def keyboard_with_user_menu(master_id, counter, url_to_reviews, url_to_works, url_to_master, city_id):
-    inline_btn_1 = InlineKeyboardButton('Записаться', callback_data=f'ms_cont_{master_id}', url='t.me/'+url_to_master[1:])
+def keyboard_with_user_menu(menu):
+    inline_btn_1 = InlineKeyboardButton('Записаться', callback_data=f'ms_cont_{menu.master_data["id"]}',
+                                        url='t.me/' + menu.master_data['telegram_user_name'][1:])
     inline_btn_2 = InlineKeyboardButton('Работы мастера',
-                                        callback_data=f'ms_port_{master_id}', url=url_to_works)
-    inline_btn_3 = InlineKeyboardButton('Отзывы', callback_data=f'ms_reviews_{master_id}', url=url_to_reviews)
-    inline_btn_4 = InlineKeyboardButton('Партнеры', callback_data=f'partners_mn_{city_id}')
+                                        callback_data=f'ms_port_{menu.master_data["id"]}',
+                                        url=menu.master_data['master_portfolio_url'])
+    inline_btn_3 = InlineKeyboardButton('Отзывы', callback_data=f'ms_reviews_{menu.master_data["id"]}',
+                                        url=menu.master_data['reviews_url'])
+    inline_btn_4 = InlineKeyboardButton('Партнеры', callback_data=f'partners_mn_{menu.master_data["location_id"]}')
 
     row2 = [types.InlineKeyboardButton('⏪', callback_data=f'previous_user_menu_'),
-            types.InlineKeyboardButton(f'{counter}', callback_data='__'),
+            types.InlineKeyboardButton(f'{menu.count_numbers()}', callback_data='__'),
             types.InlineKeyboardButton('⏩', callback_data=f'next_user_menu_')]
 
     inline_full = types.InlineKeyboardMarkup(row_width=3)
@@ -190,7 +193,8 @@ def keyboard_with_user_menu(master_id, counter, url_to_reviews, url_to_works, ur
 
 
 def keyboard_with_user_partner_menu(master_id, counter, url_to_reviews, url_to_works, url_to_partner):
-    inline_btn_1 = InlineKeyboardButton('Записаться', callback_data=f'ms_cont_{master_id}', url='t.me/'+url_to_partner[1:])
+    inline_btn_1 = InlineKeyboardButton('Записаться', callback_data=f'ms_cont_{master_id}',
+                                        url='t.me/' + url_to_partner[1:])
     inline_btn_2 = InlineKeyboardButton('Работы мастера',
                                         callback_data=f'ms_port_{master_id}', url=url_to_works)
     inline_btn_3 = InlineKeyboardButton('Отзывы', callback_data=f'ms_reviews_{master_id}', url=url_to_reviews)

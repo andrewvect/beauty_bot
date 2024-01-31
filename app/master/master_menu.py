367d92f92e93609d9ec3abdcd3bf6e58e28c9789
andrewvect <andrewvect@gmail.com>
2024-01-31 15:54:50 +0100

    code refactoring

--- app/master/master_menu.py
+++ app/master/master_menu.py
@@ -1,10 +1,5 @@
-"""
-master menu with mailing
-"""
-import traceback
-
 from sqlalchemy.orm import sessionmaker
-
+from beauty_bot.app.apps_tools.file_saver import save_image_and_get_path
 from beauty_bot.app.apps_tools.message_deleter import delete_previous_messages
 from beauty_bot.app.db_queries import get_master_photo_name_by_telegram_username, \
     get_all_master_subscribers_by_master_telegram_username, get_key_by_telegram_username
@@ -13,8 +8,6 @@ from beauty_bot.app.models import engine
 from beauty_bot.app.tools import QueriesToDb
 from beauty_bot.extantions import bot
 from beauty_bot.app.keyboards import keyboard_master_menu, keyboard_to_srart_mailing, keyboard_with_mailing_type
-from beauty_bot.app.logger import log_error
-from beauty_bot.app.services import generate_key
 
 new_masters_mailing_state = dict()
 Session = sessionmaker(bind=engine)
@@ -22,46 +15,35 @@ queries_to_db = QueriesToDb(Session)
 
 
 @delete_previous_messages
-def send_keyboard_with_master_menu(message):
+def send_keyboard_with_master_menu(message) -> None:
     bot.send_message(message.chat.id, "Личный кабинет мастера 🧑:", reply_markup=keyboard_master_menu())
 
 
 @delete_previous_messages
-def send_keyboard_with_mailing_type(call):
+def send_keyboard_with_mailing_type(call) -> None:
     new_masters_mailing_state[call.message.chat.id] = {}
 
     bot.send_message(call.message.chat.id, "Выберите тип рассылки:", reply_markup=keyboard_with_mailing_type())
 
 
 @delete_previous_messages
-def process_answer_for_new_mailing_with_photo(call):
+def process_answer_for_new_mailing_with_photo(call) -> None:
     bot.send_message(call.message.chat.id, "Пришлите фото для рассылки")
 
     bot.register_next_step_handler(call.message, process_save_photo_for_new_mailing)
 
 
 @delete_previous_messages
-def process_answer_for_new_mailing_without_photo(call):
+def process_answer_for_new_mailing_without_photo(call) -> None:
     bot.send_message(call.message.chat.id, "Пришлите описание для рассылки")
     bot.register_next_step_handler(call.message, process_save_description_for_new_mailing)
 
 
 @delete_previous_messages
-def process_save_photo_for_new_mailing(message):
-    photo = message.photo[-1]
-    file_id = photo.file_id
+def process_save_photo_for_new_mailing(message) -> None:
+    file_name = save_image_and_get_path(message, bot)
 
-    file_info = bot.get_file(file_id)
-    file_path = file_info.file_path
-
-    downloaded_file = bot.download_file(file_path)
-
-    file_name = generate_key(7)
-
-    with open(f'beauty_bot/app/photos/{file_name}.jpg', 'wb') as new_file:
-        new_file.write(downloaded_file)
-
-    new_masters_mailing_state[message.chat.id]['photo'] = file_name
+    new_masters_mailing_state[message.chat.id]['photo'] = file_name[:-4]
 
     bot.send_message(message.chat.id, "Пришлите описание для рассылки")
 
@@ -69,31 +51,25 @@ def process_save_photo_for_new_mailing(message):
 
 
 @delete_previous_messages
-def process_save_description_for_new_mailing(message):
-
+def process_save_description_for_new_mailing(message) -> None:
     new_masters_mailing_state[message.chat.id]['description'] = message.text
 
     message_text = f"{new_masters_mailing_state[message.chat.id]['description']} \n" \
                    f"Контакт: @{message.from_user.username}"
 
     if len(new_masters_mailing_state[message.chat.id]) == 1:
-        photo_master_name = get_master_photo_name_by_telegram_username('@' + message.chat.username)
-        new_masters_mailing_state[message.chat.id]['photo'] = photo_master_name
+        photo_master_from_db = get_master_photo_name_by_telegram_username('@' + message.chat.username)
+        new_masters_mailing_state[message.chat.id]['photo'] = photo_master_from_db
 
-        with open(f"beauty_bot/app/photos/{photo_master_name + '.jpg'}", 'rb') as photo:
-            bot.send_photo(message.chat.id, photo, caption=message_text)
-
-    else:
-        with open(f"beauty_bot/app/photos/{new_masters_mailing_state[message.chat.id]['photo'] + '.jpg'}",
-                  'rb') as photo:
-            bot.send_photo(message.chat.id, photo, caption=message_text)
+    with open(f"beauty_bot/app/photos/{new_masters_mailing_state[message.chat.id]['photo'] + '.jpg'}",
+              'rb') as photo:
+        bot.send_photo(message.chat.id, photo, caption=message_text)
 
     bot.send_message(message.chat.id, 'Управление рассылкой', reply_markup=keyboard_to_srart_mailing())
 
 
 @delete_previous_messages
-def start_mailing(call):
-
+def start_mailing(call) -> None:
     subscribers_telegram_ids = get_all_master_subscribers_by_master_telegram_username(call.message.chat.username)
 
     if not subscribers_telegram_ids:
@@ -101,19 +77,17 @@ def start_mailing(call):
 
     if not queries_to_db.check_master_visability(call.message.chat.username):
         return bot.send_message(call.message.chat.id, 'Ваша анкета сейчас не активна, расслыка не возможна')
+
     else:
-        try:
-            bot.send_message(call.message.chat.id, "Рассылка запущена")
 
-            for user_id in subscribers_telegram_ids:
-                send_menu_with_questionarties_by_type(call.message,
-                                                      new_masters_mailing_state[call.message.chat.id]['description'],
-                                                      new_masters_mailing_state[call.message.chat.id]['photo'],
-                                                      user_id,
-                                                      call.message.chat.username)
+        bot.send_message(call.message.chat.id, "Рассылка запущена")
 
-        except Exception:
-            print(traceback.print_exc())
+        for user_id in subscribers_telegram_ids:
+            send_menu_with_questionarties_by_type(call.message,
+                                                  new_masters_mailing_state[call.message.chat.id]['description'],
+                                                  new_masters_mailing_state[call.message.chat.id]['photo'],
+                                                  user_id,
+                                                  call.message.chat.username)
 
     bot.send_message(call.message.chat.id, "Личный кабинет мастера 🧑:", reply_markup=keyboard_master_menu())
 

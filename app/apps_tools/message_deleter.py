a8f6795dbd6215922724a02c7fc74caf6e3cce91
andrewvect <andrewvect@gmail.com>
2024-01-31 14:10:38 +0100

    debug decorator

--- app/apps_tools/message_deleter.py
+++ app/apps_tools/message_deleter.py
@@ -1,15 +1,18 @@
-import traceback
-
 from beauty_bot.extantions import bot
 
 
 def delete_previous_messages(func):
     def wrapped(*args, **kwargs):
-        call = args[0]
+
+        if hasattr(args[0], 'message'):
+            message = args[0].message
+        else:
+            message = args[0]
+
         count = 0
         while True:
             try:
-                bot.delete_message(call.message.chat.id, call.message.id - count)
+                bot.delete_message(message.chat.id, message.id - count)
                 count += 1
             except Exception:
                 break

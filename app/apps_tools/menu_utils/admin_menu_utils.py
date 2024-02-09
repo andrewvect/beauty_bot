61f8a599b5f85e18b588a669ed35a52a61d6563b
andrewvect <andrewvect@gmail.com>
2024-02-09 17:02:27 +0100

    code refactoring

---
+++ app/apps_tools/menu_utils/admin_menu_utils.py
@@ -0,0 +1,23 @@
+# menu_utils.py
+from beauty_bot.app.admin.service import send_menu_with_questionaries
+from beauty_bot.app.db_queries import set_master_active_profile
+
+
+def send_menu_with_items(call, menu_type, menu_instance):
+    menu_instance.get_all_items()
+    send_menu_with_questionaries(call, menu_type, 'new', menu_instance)
+
+
+def next_page_admin_menu(call, menu_instance):
+    menu_instance.up_page()
+    send_menu_with_questionaries(call, call.data[len('ad_next_page_'):], 'old', menu_instance)
+
+
+def previous_page_admin_menu(call, menu_instance):
+    menu_instance.down_page()
+    send_menu_with_questionaries(call, call.data[len('ad_previous_page_'):], 'old', menu_instance)
+
+
+def change_visibility(call, menu_type, menu_instance):
+    set_master_active_profile(master_id=call.data[len('acv_') + 1:])
+    send_menu_with_questionaries(call, menu_type, 'old', menu_instance)

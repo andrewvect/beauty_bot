3b10a7687e60e1af4206ceee977c3992b5647d39
andrewvect <andrewvect@gmail.com>
2024-02-14 15:22:16 +0100

    code refactoring

--- app/admin/menu_with_partners.py
+++ app/admin/menu_with_partners.py
@@ -1,26 +1,23 @@
-from sqlalchemy.orm import sessionmaker
-from beauty_bot.app.apps_tools.menu_utils.admin_menu_utils import send_menu_with_items, next_page_admin_menu, \
+from beauty_bot.app.app_tools.menu_utils.admin_menu_utils import send_menu_with_items, next_page_admin_menu, \
     previous_page_admin_menu, change_visibility
-from beauty_bot.app.models import engine
-from beauty_bot.app.tools import AdminMenuPartners, QueriesToDb
+from beauty_bot.app.app_tools.mailing_engine import AdminMenuPartners
+from beauty_bot.app.app_tools.db_queries import db
 
-Session = sessionmaker(bind=engine)
-queries_to_db = QueriesToDb(Session)
-menu_with_partners = AdminMenuPartners(queries_to_db)
+menu_with_partners = AdminMenuPartners(db)
 
 
-def send_menu_with_masters(call):
+def send_menu_with_partners(call):
     send_menu_with_items(call, 'partners', menu_with_partners)
 
 
-def next_page_masters_admin_menu(call):
+def next_page_partners_admin_menu(call):
     next_page_admin_menu(call, menu_with_partners)
 
 
-def previous_page_masters_admin_menu(call):
+def previous_page_partners_admin_menu(call):
     previous_page_admin_menu(call, menu_with_partners)
 
 
-def change_master_visibility(call):
+def change_partner_visibility(call):
     change_visibility(call, 'partners', menu_with_partners)
 

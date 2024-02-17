510f99af255bbe99d011675a2377af83217da33f
andrewvect <andrewvect@gmail.com>
2024-02-17 18:37:50 +0100

    code refactoring

--- test_end_to_end_beauty_bot/config.py
+++ test_end_to_end_beauty_bot/config.py
@@ -1,6 +1,7 @@
 import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
 DOTENV = os.path.join(os.path.dirname(__file__), "test_configuration.env")
 
@@ -12,6 +13,8 @@ class TestConfig(BaseSettings):
     api_hash: str
 
     model_config = SettingsConfigDict(env_file=DOTENV)
+    main_script_path: Path = Path(__file__).parent.parent / "app/telegram_handlers.py"
+    test_db_path: Path = Path(__file__).parent.parent / "beauty_bot1.db"
 
 
 TEST_CONFIG = TestConfig()

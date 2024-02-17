44f207370416f77fc65e70f82a91df6f6b4df663
andrewvect <andrewvect@gmail.com>
2024-02-17 18:37:32 +0100

    code refactoring

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -16,12 +15,4 @@ class AppConfig(BaseSettings):
 
 CONFIG = AppConfig()
 
-# Logging.
-logger = logging.getLogger(__name__)
-logger.setLevel('DEBUG')
-
-handler = logger.handlers and logger.handlers[0] or logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(levelname)s [%(filename)s:%(lineno)s] %(message)s'))
-if not logger.handlers:
-    logger.addHandler(handler)
 

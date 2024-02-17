a26a2c381fbd7a9196c7b07e7808350473e61a76
andrewvect <andrewvect@gmail.com>
2024-02-17 18:38:09 +0100

    code refactoring

--- app/app_tools/keyboards/MasterKeyboards.py
+++ app/app_tools/keyboards/MasterKeyboards.py
@@ -1,16 +1,17 @@
 from beauty_bot.app.app_tools.keyboards.Base import BaseKeyboards
+from beauty_bot.app.app_tools.keyboards.CallBackButtons import MasterCallBackButtons
 
 
-class MasterKeyboards(BaseKeyboards):
+class MasterKeyboards(BaseKeyboards, MasterCallBackButtons):
 
     def keyboard_to_start_mailing_as_master(self):
-        return super().keyboard_to_start_mailing('start_mailing', 'return_to_master_menu')
+        return super().keyboard_to_start_mailing(self.button_to_start_mailing, self.button_to_return_to_menu)
 
     def keyboard_master_menu(self):
-        return super().keyboard_to_start_mailing('mailing', 'out_as_ms')
+        return super().keyboard_to_start_mailing(self.button_open_mailing, self.button_out_of_mailing)
 
     def keyboard_with_mailing_as_master(self):
-        return super().keyboard_mailing('mailing_with_photo', 'mailing_without_photo')
+        return super().keyboard_mailing(self.button_mailing_with_photo, self.button_mailing_without_photo)
 
 
 master_keyboards = MasterKeyboards()
